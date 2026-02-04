97935f9a06a7df3b7f3f3e7a45b457dda03e1634
Azad <azaddogan118@gmail.com>
2026-02-04 22:42:11 +0300

    Fix API_URL format in app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=9a4e36e2f6a7344fdbac25950248aaf2&language=tr-TR`;
+const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key==tr-TR`;
 
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 
